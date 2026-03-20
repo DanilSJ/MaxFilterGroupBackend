989c77bc75d01e5c7a231736eb061a38daee4a50
DanilSJ <danilchagarnoy@mail.ru>
2026-03-20 21:35:50 +0300

    add model group, grid

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,10 +1,14 @@
 __all__ = [
     "Base",
     "User",
+    "Grid",
+    "Group",
     "DatabaseHelper",
     "db_helper",
 ]
 
 from .base import Base
 from .user import User
+from .grid import Grid
+from .group import Group
 from .db_helper import DatabaseHelper, db_helper
