48129967b5d41225e26a14278401c9d15d23f2da
DanilSJ <danilchagarnoy@mail.ru>
2026-03-20 21:52:04 +0300

    create update_group

--- api_v1/group/crud.py
+++ api_v1/group/crud.py
@@ -2,19 +2,41 @@ from fastapi import HTTPException
 from sqlalchemy.engine import Result
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
-from core.models import User
+from core.models import Group
 from .schemas import *
 
 
-async def create_group(session: AsyncSession, user_in: CreateGroupSchema) -> User:
-    stmt = select(User).where(User.name == user_in.name)
+async def create_group(session: AsyncSession, group_in: CreateGroupSchema) -> Group:
+    stmt = select(Group).where(Group.name == group_in.name)
     result: Result = await session.execute(stmt)
 
     if result.scalars().first():
         raise HTTPException(status_code=409, detail="Group already exists")
 
-    group = User(**user_in.model_dump())
+    group = Group(**group_in.model_dump())
     session.add(group)
     await session.commit()
     return group
 
+
+async def update_group(
+        session: AsyncSession,
+        group_id: int,
+        group_update: UpdateGroupSchemaPartial
+):
+    stmt = select(Group).where(Group.id == group_id)
+    result: Result = await session.execute(stmt)
+    group = result.scalars().first()
+
+    if not group:
+        raise HTTPException(status_code=404, detail="Group not found")
+
+    update_data = group_update.model_dump(exclude_unset=True)
+
+    for field, value in update_data.items():
+        setattr(group, field, value)
+
+    await session.commit()
+    await session.refresh(group)
+
+    return group
