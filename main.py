8b178294a1ab975591c09c308af5105520021295
DanilSJ <danilchagarnoy@mail.ru>
2026-03-20 21:13:06 +0300

    create fastapi app

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from maxfiltergroupbackend!")
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+import uvicorn
 
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield
+
+
+app = FastAPI(
+    lifespan=lifespan,
+)
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run(app, host="127.0.0.1", port=7777)
