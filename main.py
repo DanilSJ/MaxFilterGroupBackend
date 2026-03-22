044f7e10de3300369768ad3ff7d52e05f16e504e
DanilSJ <danilchagarnoy@mail.ru>
2026-03-22 23:47:31 +0300

    add CORS

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import uvicorn
 from api_v1.user.views import router as user_router
 from api_v1.group.views import router as group_router
 from api_v1.grid.views import router as grid_router
+from fastapi.middleware.cors import CORSMiddleware
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -14,9 +15,18 @@ app = FastAPI(
     lifespan=lifespan,
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 app.include_router(user_router, tags=["user"], prefix="/user")
 app.include_router(group_router, tags=["group"], prefix="/group")
 app.include_router(grid_router, tags=["grid"], prefix="/grid")
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=7777)
+    uvicorn.run(app, host="0.0.0.0", port=7777)
