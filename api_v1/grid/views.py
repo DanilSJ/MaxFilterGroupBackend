e673f1247cd8491b89b387f1f4caa3cfb9ac6edb
DanilSJ <danilchagarnoy@mail.ru>
2026-03-22 23:47:09 +0300

    create endpoint grids get_grid_group

--- api_v1/grid/views.py
+++ api_v1/grid/views.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from core.models import db_helper
@@ -8,25 +10,35 @@ from api_v1.auth.views import get_token, get_current_user
 router = APIRouter()
 
 
+@router.get("s/", response_model=List[schemas.GridSchema])
+async def grids(
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency),
+):
+
+    return await crud.get_grids(session=session)
+
 @router.post("/", response_model=schemas.GridSchema)
 async def create_grid(
     grid_in: schemas.CreateGridSchema,
-    token: str = Depends(get_token),
     session: AsyncSession = Depends(db_helper.scoped_session_dependency),
 ):
-    await get_current_user(token)
-
     return await crud.create_grid(session=session, grid_in=grid_in)
+
+@router.get("/{grid_id}/", response_model=schemas.GridSchema)
+async def get_grid_group(
+    grid_id: int,
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency),
+):
+
+    return await crud.get_grid_group(session=session, grid_id=grid_id)
 
 
 @router.patch("/{group_id}/", response_model=schemas.GridSchema)
 async def update_grid(
         group_id: int,
         group_update: schemas.UpdateGridSchemaPartial,
-        token: str = Depends(get_token),
         session: AsyncSession = Depends(db_helper.scoped_session_dependency),
 ):
-    await get_current_user(token)
 
     return await crud.update_grid(
         session=session,
