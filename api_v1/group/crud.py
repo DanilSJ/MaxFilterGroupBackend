bea5394abc31ce68f160608df509c5462516fee1
DanilSJ <danilchagarnoy@mail.ru>
2026-03-24 15:54:49 +0300

    create func delete_group

--- api_v1/group/crud.py
+++ api_v1/group/crud.py
@@ -48,3 +48,15 @@ async def update_group(
     await session.refresh(group)
 
     return group
+
+
+async def delete_group(session: AsyncSession, group_id: int) -> bool:
+    stmt = select(Group).where(Group.group_id == group_id)
+    result: Result = await session.execute(stmt)
+    group = result.scalar_one_or_none()
+
+    if group:
+        await session.delete(group)
+        await session.commit()
+        return True
+    return False
