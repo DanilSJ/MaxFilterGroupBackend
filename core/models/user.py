75953285923360b216aded04b0a161f85a35d5ea
DanilSJ <danilchagarnoy@mail.ru>
2026-04-20 22:26:28 +0300

    add field max_id, blocked_in_grids

--- core/models/user.py
+++ core/models/user.py
@@ -1,10 +1,20 @@
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, Integer
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from .associations_grid_user import grid_block_users
 from .base import Base
 
 
 class User(Base):
-    email: Mapped[str] = mapped_column(String)
-    password: Mapped[str] = mapped_column(String)
+    max_id: Mapped[int] = mapped_column(Integer, nullable=True)
+
+    email: Mapped[str] = mapped_column(String, nullable=True)
+    password: Mapped[str] = mapped_column(String, nullable=True)
 
     admin: Mapped[bool] = mapped_column(Boolean, default=False)
+
+    blocked_in_grids: Mapped[list["Grid"]] = relationship(
+        "Grid",
+        secondary=grid_block_users,
+        back_populates="block_users"
+    )
